17cc8648b774f3adf96919ee18316f2ea9c57da0
Edgar Sun <esun@voteb.com>
2023-01-19 02:25:49 +0900

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unity-grpc-build-pipeline',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     description='grpc pipeline interfaces',
     author='esun',
