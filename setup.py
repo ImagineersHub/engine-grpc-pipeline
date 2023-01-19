a65c58299116ad7e5cd663b78f6ec3a526d010af
Edgar Sun <esun@voteb.com>
2023-01-19 13:00:17 +0900

    updated setup deps

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unity-grpc-build-pipeline',
-    version='0.0.7',
+    version='0.0.8',
     license='MIT',
     description='grpc pipeline interfaces',
     author='esun',
@@ -15,8 +15,8 @@ setup(
         'grpcio-tools==1.50.0',
         'protobuf==4.21.8',
         'betterproto[compiler]>=2.0.*',
-        'git+https://github.com/ImagineersHub/unity-grpc-build-proto-pipe.git@main#egg=ugrpc_pipe',
-        'git+https://github.com/ImagineersHub/compipe.git@main#egg=compipe'
+        'ugrpc_pipe @ git+https://github.com/ImagineersHub/unity-grpc-build-proto-pipe.git@main#egg=ugrpc_pipe',
+        'compipe @ git+https://github.com/ImagineersHub/compipe.git@main#egg=compipe'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
