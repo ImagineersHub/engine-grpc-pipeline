440b26bfec870a9a8f71a6f06d4da95b1f59ad85
Edgar Sun <esun@voteb.com>
2023-01-19 12:23:10 +0900

    updated utils folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unity-grpc-build-pipeline',
-    version='0.0.4',
+    version='0.0.6',
     license='MIT',
     description='grpc pipeline interfaces',
     author='esun',
