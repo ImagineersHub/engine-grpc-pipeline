90dd75177ed818d10191f50ef788366e6bf80858
Edgar Sun <esun@voteb.com>
2023-01-19 13:16:50 +0900

    updated dep

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='unity-grpc-build-pipeline',
-    version='0.0.11',
+    version='0.0.12',
     license='MIT',
     description='grpc pipeline interfaces',
     author='esun',
     author_email='esun@voteb.com',
-    url='https://github.com/ImagineersHub/unity-grpc-build-pipeline',
+    url='https://github.com/ImagineersHub/engine-grpc-pipeline',
     keywords=['python', 'grpc', 'unity', 'unreal'],
     packages=find_packages(),
     install_requires=[
