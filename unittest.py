4dbaf3fcc99e58c970b180d7d5861f06f215fcb8
Edgar Sun <esun@voteb.com>
2023-02-05 00:43:21 +0900

    added unitest

---
+++ unittest.py
@@ -0,0 +1,16 @@
+from engine_grpc import UEI, GI
+from compipe.runtime_env import Environment
+from compipe.utils.io_helper import json_loader
+import os
+import sys
+
+# initialize env and append server config from local json
+server_config_path = os.path.join(os.path.dirname(__file__), 'debug_runtime_config.json')
+server_config = json_loader(path=server_config_path).get(sys.platform or 'win32')
+Environment.append_server_config(payload=server_config)
+
+
+if __name__ == "__main__":
+    result = UEI().find_assets(filter="", paths=["Assets/Content"])
+
+    print(result)
