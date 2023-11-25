c48753878b3b946be1a8f77266f7f458bc215340
esun <esun@voteb.com>
2023-11-25 10:32:02 +0900

    added unitest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='engine_grpc',
-    version='0.1.12',
+    version='0.1.13',
     license='MIT',
     description='grpc pipeline interfaces',
     author='esun',
--- unittest.py
+++ unittest.py
@@ -3,19 +3,17 @@ from compipe.runtime_env import Environment
 from compipe.utils.io_helper import json_loader
 import os
 import sys
-from ugrpc_pipe import FloatArrayRep
+from ugrpc_pipe import FloatArrayRep, GenericResp
 # initialize env and append server config from local json
-server_config_path = os.path.join(os.path.dirname(__file__), 'debug_runtime_config.json')
-server_config = json_loader(path=server_config_path).get(sys.platform or 'win32')
+server_config_path = os.path.join(
+    os.path.dirname(__file__), 'debug_runtime_config.json')
+server_config = json_loader(
+    path=server_config_path).get(sys.platform or 'win32')
 Environment.append_server_config(payload=server_config)
 
 
 if __name__ == "__main__":
     result = UEI().command_parser(
-        cmd=GI.method_scene_create,
-        params=[
-            "Assets/Content/Scene/test.unity",
-            []
-        ])
-
-    print(result.values)
+        cmd=GI.method_system_get_service_status,
+        params=[], return_type=GenericResp)
+    print(result.status)
