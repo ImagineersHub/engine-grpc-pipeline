d2c223f0df13ae701060848d2101a1ff2702220d
FifthDimensionDev <66546873+esunvoteb@users.noreply.github.com>
2024-01-22 13:57:36 +0900

    Update setup.py
    update protobuf version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='engine_grpc',
-    version='0.2.1',
+    version='0.2.2',
     license='MIT',
     description='grpc pipeline interfaces',
     long_description=open("README.md").read(),
@@ -15,7 +15,7 @@ setup(
     install_requires=[
         'grpcio==1.50.0',
         'grpcio-tools==1.50.0',
-        'protobuf==4.21.8',
+        'protobuf==4.25.2',
         'betterproto[compiler]==2.0.0b5',
         'ugrpc_pipe==0.1.1',
         'compipe>=0.2.3'
