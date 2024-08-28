6dd90cac2a7d38ae59fc506a4382d07be8faa6c8
esun <esun@voteb.com>
2024-08-28 23:25:40 +0900

    removed port incremental behavior

--- engine_grpc/engine_pipe_channel.py
+++ engine_grpc/engine_pipe_channel.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import asyncio
 from dataclasses import dataclass
-
+from compipe.utils.singleton import Singleton
 from compipe.runtime_env import Environment as env
 from compipe.utils.logging import logger
 from grpclib.client import Channel
@@ -10,6 +10,19 @@ from grpclib.config import Configuration
 from ugrpc_pipe import UGrpcPipeStub
 
 from .engine_pipe_abstract import EngineAbstract
+
+
+class GrpcCacheConfig(metaclass=Singleton):
+    host: str = None
+    port: int = 0
+
+    def update(self, host: str, port: int):
+        self.host = host
+        self.port = port
+
+    @property
+    def channel(self):
+        return None if self.host == None or self.port == 0 else f"{self.host}:{self.port}"
 
 
 @dataclass
@@ -38,7 +51,7 @@ class base_channel(object):
         self.grpc_cfg: GrpcChannelConfig = GrpcChannelConfig.retrieve_grpc_cfg(
             engine=self.engine.engine_platform)
         if not self.channel:
-            self.grpc_cfg = self.channel
+            self.channel = self.grpc_cfg.channel
             # logger.warning(f"Initialize gRPC channel by passing default values {self.channel}")
 
         if ':' not in self.channel:
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='engine_grpc',
-    version='0.2.22',
+    version='0.2.23',
     license='MIT',
     description='grpc pipeline interfaces',
     long_description=open("README.md").read(),
