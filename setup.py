760d38e7dae83391c299e7aa63bf4fcd13ee180c
esun <esun@voteb.com>
2025-06-11 00:37:58 +0900

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='engine_grpc',
-    version='0.3.1',
+    version='0.3.2',
     license='MIT',
     description='grpc pipeline interfaces',
     long_description=open("README.md").read(),
